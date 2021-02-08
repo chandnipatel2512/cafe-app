09f88c353e0e44db6acc14379ec736e89ad5cd9c
Chandni <chandnipatel2512@gmail.com>
2021-02-08 19:57:32 +0000

    Test create item function

--- tests/test_items.py
+++ tests/test_items.py
@@ -2,8 +2,9 @@ from src.functions.list_functions import number_items, create, update, delete
 from unittest.mock import Mock
 import unittest
 
-# Test create function to ensure it adds items to specified list
+
 class Testing(unittest.TestCase):
+    # Test create function to ensure it adds items to specified list
     def test_create(self):
         empty = []
         mock_input = Mock()
@@ -15,11 +16,23 @@ class Testing(unittest.TestCase):
         actual = create("product", "price", empty, mock_input)
         assert expected == actual
 
+        # Test create function to ensure it cancels and returns "" when user want to cancel and return to operations menu
+
+    def test_create_cancel(self):
+        empty = []
+        mock_input = Mock()
+        mock_input.side_effect = ["0"]
+
+        test_product = []
+
+        expected = test_product
+        actual = create("courier", "phone", empty, mock_input)
+        assert expected == actual
+
 
 if __name__ == "__main__":
     unittest.main()
 
-# Test create function to ensure it cancels and returns "" when user want to cancel and return to operations menu
 
 # Test update function to ensure it returns updated list when user want to cancel and return to operations menu
 
