75c4eb0ea3c4013a649702cebdd1caeadaa71c5e
Chandni <chandnipatel2512@gmail.com>
2021-02-12 21:14:18 +0000

    Update list functions

--- src/functions/list_functions.py
+++ src/functions/list_functions.py
@@ -1,3 +1,11 @@
+# Number items in list:
+def number_items(list, index=1):
+    for item in list:
+        print(index, item)
+        index += 1
+    return ""
+
+
 # List key names:
 def key_names(list_name=[]):
     return list(list_name[0].keys())
@@ -8,19 +16,99 @@ def value_types(list_name=[]):
     return [type(v) for v in list_name[0].values()]
 
 
-# Function for string input with cancellation option
+# List of all values in list of dictionaries for specific key
+def list_values(list_name=[], key_name=""):
+    return [d[key_name] for d in list_name]
+
+
+# Function for a string input with cancellation option
 # Return to be updated
-def string_with_cancel(key_name=""):
+def string_with_cancel(list_name=[], key_name=""):
+    existing_items = list_values(list_name, key_name)
     while True:
         user_input = input(
             f"\nPlease enter the {key_name}. Alternatively, please enter 0 to cancel.\n"
         )
         if not user_input:
             print(f"\nPlease enter a valid {key_name}")
+        elif user_input in existing_items:
+            print(f"\nThis {key_name} already exists.")
         else:
             break
+    return user_input
 
-    if user_input != "0":
-        return user_input
-    else:
-        return "main_menu"
+
+# Function for a string input
+def string_input(list_name=[], key_name=""):
+    existing_items = list_values(list_name, key_name)
+    while True:
+        user_input = input(f"\nPlease enter the {key_name}.\n")
+        if not user_input:
+            print(f"\nInvalid input")
+        elif user_input in existing_items:
+            print(f"\nThis {key_name} already exists.")
+        else:
+            break
+    return user_input
+
+
+# Function for an integer input
+def integer_input(list_name=[], key_name=""):
+    while True:
+        try:
+            user_input = int(input(f"\nPlease enter the {key_name}.\n"))
+            if not user_input or user_input < 0:
+                raise ValueError
+        except (ValueError, IndexError):
+            print(f"\nInvalid input")
+        else:
+            break
+    return user_input
+
+
+# Function for a float input
+def float_input(list_name=[], key_name=""):
+    while True:
+        try:
+            user_input = float(input(f"\nPlease enter the {key_name}.\n"))
+            if not user_input or user_input < 0:
+                raise ValueError
+        except (ValueError, IndexError):
+            print(f"\nInvalid input")
+        else:
+            break
+    return user_input
+
+
+# Function for selecting an item from a list
+def select_item(list_name=[], external_list=[], key_name=""):
+    external_items = list_values(external_list, key_name)
+    print(number_items(external_items))
+    while True:
+        try:
+            user_input = int(input(f"\nPlease select an id number.\n"))
+            list_item = external_items[user_input - 1]
+            if not user_input or user_input <= 0:
+                raise ValueError
+        except (ValueError, IndexError):
+            print(f"\nInvalid input")
+        else:
+            break
+    return list_item
+
+
+# Function for order status
+def order_status():
+    status_options = ["Order placed", "Preparing", "Being delivered", "Delivered"]
+    number_items(status_options)
+    while True:
+        try:
+            user_input = int(input(f"\nPlease select the current order status.\n"))
+            current_status = status_options[user_input - 1]
+            if not user_input or user_input <= 0:
+                raise ValueError
+        except (ValueError, IndexError):
+            print(f"\nInvalid input")
+        else:
+            break
+    return status_options[user_input - 1]
--- src/functions/operational_functions.py
+++ src/functions/operational_functions.py
@@ -1,165 +1,52 @@
-from .file_functions import load_data, save_data
-
-# Number items in list:
-def number_items(list, index=1):
-    for item in list:
-        print(index, item)
-        index += 1
-    return ""
-
+from src.functions.list_functions import (
+    number_items,
+    key_names,
+    value_types,
+    string_with_cancel,
+    string_input,
+    integer_input,
+    float_input,
+    select_item,
+    order_status,
+)
 
 # Create new item in list
-def create(item="", detail="", list=[], input=input):
-
-    new_item = input(
-        f"\nPlease provide the name of the {item} you would like to add to the menu, alternatively, please enter 0 to cancel.\n"
-    )
-    if new_item != "0":
-        if detail == "price":
-            while True:
-                try:
-                    item_detail = float(
-                        input(f"\nPlease provide the price of the product.\n")
-                    )
-                except ValueError:
-                    print("\nPlease enter a valid price.")
-                    continue
-                else:
-                    break
+def create(list=[], external_list=[]):
+    keys = key_names(list)
+    value_type = value_types(list)
+    new_item = {}
+    index = 0
+    for key in keys:
+        if index == 0:
+            input = string_with_cancel(list, key)
+            if input == 0:
+                return
+            else:
+                new_item[key] = input
+        elif key == "courier":
+            new_item[key] = select_item(list, external_list, "name")
+        elif key == "order status":
+            new_item[key] = order_status()
+        elif value_type[index] == int:
+            new_item[key] = integer_input(list, key)
+        elif value_type[index] == float:
+            new_item[key] = float_input(list, key)
         else:
-            item_detail = input(
-                f"\nPlease provide the contact number of the courier.\n"
-            )
-        list.append(dict({"name": new_item, detail: item_detail}))
-    return list
+            new_item[key] = string_input(list, key)
+        index += 1
+    return list.append(new_item)
 
 
 # Update item in list
-def update(item="", detail="", list=[], input=input):
-    number_items(list)
-    while True:
-        try:
-            item_number = int(
-                input(
-                    f"\nPlease provide the number of the {item} you would like to update, alternatively, please enter 0 to cancel.\n"
-                )
-            )
-            list_item = list[item_number - 1]
-        except (ValueError, IndexError):
-            print("\nPlease enter a valid number.")
-        else:
-            break
-    if item_number != 0:
-        updated_name = input(f"\nPlease provide the updated {item} name.\n")
-        if detail == "price":
-            while True:
-                try:
-                    item_detail = float(
-                        input(f"\nPlease provide the updated product price.\n")
-                    )
-                except ValueError:
-                    print("\nPlease enter a valid price.")
-                    continue
-                else:
-                    break
-        else:
-            item_detail = input(
-                f"\nPlease provide the updated contact number of the courier.\n"
-            )
-    list[item_number - 1]["name"] = updated_name
-    list[item_number - 1][detail] = item_detail
-    return list
 
 
 # Delete item in list
-def delete(item="", list=[], input=input):
-    z = input(
-        f"\nPlease provide the number of the {item} you would like to delete, alternatively, please enter 0 to cancel."
-    )
-    return "" if z == "0" else list.remove(list[int(z) - 1])
-
-
-# Add new order
-def new_order(couriers=[], orders=[], input=input):
-    name = input(
-        "Please provide the customer name, alternatively, please enter 0 to cancel."
-    )
-    if name == "0":
-        return
-    address = input("Please provide the customer address.")
-    phone_number = input("Please provide the customer phone number.")
-    number_items(couriers)
-    courier = couriers[int(input("Please select the courier for this order.") - 1)]
-    order = {
-        "customer_name": name,
-        "customer_address": address,
-        "customer_phone": phone_number,
-        "courier": courier,
-        "status": "preparing",
-    }
-    return orders.append(dict(order))
 
 
 # Update order status
-def update_order_status(orders=[], input=input):
-    number_items(orders)
-    number = int(
-        input(
-            "Please provide the order number for which you would like to update the status, alternatively, please enter 0 to cancel."
-        )
-    )
-    if number == 0:
-        return ""
-    else:
-        updated_status = input("Please provide the latest order status.")
-        orders[number - 1]["status"] = updated_status
-    return orders
 
 
 # Update order
-def update_order(orders=[], couriers=[], input=input):
-    number_items(orders)
-    number = int(
-        input(
-            "Please provide the order number you would like to update, alternatively, please enter 0 to cancel."
-        )
-    )
-    if number == 0:
-        return ""
-    else:
-        keys = orders[0].keys()
-        number_items(keys)
-        option = int(input("What would you like to update?"))
-        if option == 1:
-            name = input("Please provide the updated customer name.")
-            orders[number - 1]["customer_name"] = name
-        if option == 2:
-            address = input("Please provide the updated customer address.")
-            orders[number - 1]["customer_address"] = address
-        if option == 3:
-            phone = input("Please provide the updated contact number.")
-            orders[number - 1]["customer_phone"] = phone
-        if option == 4:
-            number_items(couriers)
-            courier = couriers[
-                int(input("Please select the new courier for this order.")) - 1
-            ]
-            orders[number - 1]["courier"] = courier
-        if option == 5:
-            updated_status = input("Please provide the latest order status.")
-            orders[number - 1]["status"] = updated_status
 
 
 # Delete order
-def delete_order(orders=[], input=input):
-    number_items(orders)
-    number = int(
-        input(
-            "Please provide the order number you would like to delete, alternatively, please enter 0 to cancel."
-        )
-    )
-    if number == 0:
-        return ""
-    else:
-        orders.remove(orders[number - 1])
-    print(orders)
